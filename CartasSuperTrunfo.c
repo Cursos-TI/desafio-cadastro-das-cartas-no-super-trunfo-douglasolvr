9acfb37c04dfe85c6219a90ecef5fb99d207ebc6
Douglas Oliveira <douglasap96@gmail.com>
2025-08-27 02:34:58 +0000

    Criei o CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
+  char estado [10];
+  char carta [4];
+  char cidade[10];
+  int populacao;
+  float area;
+  float PIB;
+  int pontos;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+  printf("Digite o Estado: \n");
+  scanf("%s", estado);
 
-    return 0;
+  printf("Digite o código da carta: \n");
+  scanf("%s", carta);
+
+  printf("Digite o nome da Cidade: \n");
+  scanf("%s", cidade);
+
+  printf("Digite a população: \n");
+  scanf("%d", &populacao);
+
+  printf("Digite a área da cidade: \n");
+  scanf("%f", &area);
+
+  printf("Digite o PIB da cidade: \n");
+  scanf("%f", &PIB);
+
+  printf("Digite o número de pontos turísticos: \n");
+  scanf("%d",&pontos);
+
+  printf("Nome do estado: %s\n", estado);
+  printf("Código da carta: %s\n", carta);
+  printf("Nome da cidade: %s\n", cidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %f\n", area);
+  printf("PIB: %f\n", PIB);
+  printf("Número de pontos turísticos: %d\n", pontos);
+
+  return 0;
 }
